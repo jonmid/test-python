a5dc3d4b6ab123d60d8e4fc868f86f0ee24b137d
Jhonatan Mideros <10jonmid@gmail.com>
2024-12-15 11:15:13 -0500

    first commit

---
+++ tests/tests_all_asserts.py
@@ -0,0 +1,49 @@
+import unittest
+
+SERVER="server_b"
+
+class AllAssertsTests(unittest.TestCase):
+
+    # Test para evaluar igualdad
+    def test_assert_equal(self):
+        self.assertEqual(10, 10)
+        self.assertEqual("Hola", "Hola")
+
+    # Test para evaluar True o False
+    def test_assert_true_or_false(self):
+        self.assertTrue(True)
+        self.assertFalse(False)
+
+    # Test para evaluar excepciones
+    def test_assert_raises(self):
+        with self.assertRaises(ValueError):
+            int("no_soy_un_numero")
+
+    # Test para evaluar si un elemento esta o no dentro de una lista "[]"
+    def test_assert_in(self):
+        self.assertIn(10, [2, 4, 5, 10])
+        self.assertNotIn(5, [2, 4, 10])
+
+    # Test para validar diccionarios
+    def test_assert_dicts(self):
+        user = {"first_name": "Luis", "last_name": "Martinez"}
+        self.assertDictEqual(
+            {"first_name": "Luis", "last_name": "Martinez"},
+            user
+        )
+        self.assertSetEqual(
+            {1, 2, 3},
+            {1, 2, 3}
+        )
+    
+    @unittest.skip("Trabajo en progreso, será habilitada nuevamente")
+    def test_skip(self):
+        self.assertEqual("hola", "by")
+
+    @unittest.skipIf(SERVER == "server_b", "Saltado porque no estamos en el servidor")
+    def test_skip_if(self):
+        self.assertEqual(100, 100)
+
+    @unittest.expectedFailure
+    def test_expected_failure(self):
+        self.assertEqual(100, 150)
---
+++ tests/tests_bank_account.py
@@ -0,0 +1,40 @@
+import unittest, os
+
+from src.bank_account import BankAccount
+
+class BankAccountTests(unittest.TestCase):
+
+    # Se ejecuta antes de iniciar las pruebas
+    def setUp(self) -> None:
+        self.account = BankAccount(balance=1000, log_file="transaction_log.txt")
+
+    # Se ejecuta después de terminar cada test
+    def tearDown(self) -> None:
+        if os.path.exists(self.account.log_file):
+            os.remove(self.account.log_file)
+
+    def _count_lines(self, filename):
+        with open(filename, "r") as f:
+            return len(f.readlines())
+    
+    # ************************************************
+
+    def test_deposit(self):
+        new_balance = self.account.deposit(500)
+        self.assertEqual(new_balance, 1500, "The balance sheet is not equal")
+
+    def test_withdraw(self):
+        new_balance = self.account.withdraw(200)
+        self.assertEqual(new_balance, 800, "The balance sheet is not equal")
+
+    def test_get_balance(self):
+        self.assertEqual(self.account.get_balance(), 1000)
+
+    def test_transaction_log(self):
+        self.account.deposit(500)
+        self.assertTrue(os.path.exists("transaction_log.txt"))
+
+    def test_count_transactions(self):
+        self.assertEqual(self._count_lines(self.account.log_file), 1)
+        self.account.deposit(500)
+        self.assertEqual(self._count_lines(self.account.log_file), 2)
