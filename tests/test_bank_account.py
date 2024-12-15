4755ef92600dde1350be2bb948042d2e0c75ba5d
Jhonatan Mideros <jmideros@robinfood.com>
2024-12-15 17:06:30 -0500

    [add] test using subTest

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -63,3 +63,18 @@ class BankAccountTests(unittest.TestCase):
         mock_datetime.now.return_value.hour = 18
         with self.assertRaises(WithdrawalTimeRestrictionError):
             self.account.withdraw(100)
+    
+    # Test con multiples valores.
+    # Este caso prueba el método "deposit" con múltiples valores usando "subTest".
+    def test_deposit_multiple_amounts(self):
+        test_cases = [
+            {"amount": 100, "expected": 1100},
+            {"amount": 3000, "expected": 4000},
+            {"amount": 4500, "expected": 5500},
+        ]
+
+        for case in test_cases:
+            with self.subTest(case=case):
+                self.account = BankAccount(balance=1000, log_file="transaction_log.txt")
+                new_balance = self.account.deposit(case["amount"])
+                self.assertEqual(new_balance, case["expected"])
